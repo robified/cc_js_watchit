6192dd2f7e9ad7bc0d11c298f66d88f9b4b486de
Robin <daorob@gmail.com>
2020-03-31 18:04:44 -0500

    Implement spawn method with node command <name of file> and properly outputting to console upon file changes.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
+const { spawn } = require('child_process');
 
 program
     .version('0.0.1')
@@ -21,7 +22,7 @@ program
         }
         
         const start = debounce(() => {
-            console.log('STARTING USER\'S PROGRAM')
+            spawn('node', [name], { stdio: 'inherit' });
         }, 100);
         
         chokidar.watch('.')
