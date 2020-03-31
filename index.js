2044fc12b9edb502f69b71c6fc5b4349565e5a39
Robin <daorob@gmail.com>
2020-03-31 02:25:58 -0500

    Install/require lodash.debounce module and successfully apply debounce to start fn to limit the number of fn calls.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 
+const start = debounce(() => {
+    console.log('STARTING USER\'S PROGRAM')
+}, 100);
+
 chokidar.watch('.')
-    .on('all', () => console.log('FILE ADDED'))
+    .on('all', start)
     .on('change', () => console.log('FILE CHANGED'))
     .on('unlink', () => console.log('FILE UNLINKED'));
