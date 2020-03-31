f26f8f208fb229fa98337064965f5462a399dd67
Robin <daorob@gmail.com>
2020-03-31 11:32:02 -0500

    Add comments to explain what to do if a file exists or not.

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const fs = require('fs');
 program
     .version('0.0.1')
     .argument('[filename]', 'Name of a file to execute')
+    // destructure filename out of the args object
     .action(async ({ filename }) => {
+        // if there is a filename, then use it. If not, then default to index.js
         const name = filename || 'index.js';
 
+        // gotta put the promise in a try/catch block
         try {
             await fs.promises.access(name);
         } catch (err) {
